17c241c96feb3286cb93e5ad578ad27f66ed8084
Trung Phat <phat.ltrung@gmail.com>
2024-04-08 20:03:12 +0700

    Setup mongodb in Docker & config to connect mongo

---
+++ src/configs/mongodb.config.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get('DB_USER')}:${configService.get('DB_PASS')}@127.0.0.1:${configService.get('DB_PORT')}/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.4`,
+        dbName: configService.get('DB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class MongoDBModule {}
